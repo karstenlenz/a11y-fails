8736e6c18b4c4451cc691c820b34d5124751fce8
Karsten Lenz <karsten.lenz@iteratec.com>
2023-06-13 22:13:53 +0200

    Add improved cookie banner

--- improved/script.js
+++ improved/script.js
@@ -1,41 +1,44 @@
-window.onload = init;
+window.onload = init
 
 function init() {
-  const menuButton = document.querySelector('#menu-btn');
-  menuButton.addEventListener('click', toggleMenu);
+    const menuButton = document.querySelector('#menu-btn')
+    menuButton.addEventListener('click', toggleMenu)
 
-  const formElement = document.getElementById('contact-form');
-  formElement.addEventListener('submit', (event) => handleSubmit(event));
+    const formElement = document.getElementById('contact-form')
+    formElement.addEventListener('submit', (event) => handleSubmit(event))
+
+    const dialogElement = document.getElementById('modal')
+    dialogElement.showModal()
 }
 
 async function handleSubmit(event) {
-  event.preventDefault();
-  const form = event.target;
+    event.preventDefault()
+    const form = event.target
 
-  const formData = new FormData(form);
-  for (var [key, value] of formData.entries()) {
-    console.log(key, value);
-  }
+    const formData = new FormData(form)
+    for (var [key, value] of formData.entries()) {
+        console.log(key, value)
+    }
 
-  await fetch('/contact', {
-    method: 'post',
-    body: formData,
-  })
-    .then((response) => response.json())
-    .then((data) => showResult(data.message));
+    await fetch('/contact', {
+        method: 'post',
+        body: formData,
+    })
+        .then((response) => response.json())
+        .then((data) => showResult(data.message))
 
-  event.target.classList.add('hidden');
+    event.target.classList.add('hidden')
 }
 
 function showResult(text) {
-  const responseElement = document.createElement('div');
-  responseElement.innerHTML = text;
+    const responseElement = document.createElement('div')
+    responseElement.innerHTML = text
 
-  const mainElement = document.querySelector('main');
-  mainElement.appendChild(responseElement);
+    const mainElement = document.querySelector('main')
+    mainElement.appendChild(responseElement)
 }
 
 function toggleMenu() {
-  const navElement = document.querySelector('.navigation');
-  navElement.classList.toggle('menu-open');
+    const navElement = document.querySelector('.navigation')
+    navElement.classList.toggle('menu-open')
 }
--- script.js
+++ script.js
@@ -1,52 +1,55 @@
-window.onload = init;
+window.onload = init
 
 function init() {
-  const menuButton = document.querySelector('#menu-btn');
-  const teaserButton = document.querySelector('#teaser-button');
-  menuButton.addEventListener('click', toggleMenu);
+    const menuButton = document.querySelector('#menu-btn')
+    const teaserButton = document.querySelector('#teaser-button')
+    menuButton.addEventListener('click', toggleMenu)
 
-  const formElement = document.getElementById('contact-form');
+    const formElement = document.getElementById('contact-form')
 
-  const submitButton = document.getElementById('submit-button');
-  submitButton.addEventListener('click', (event) => handleSubmit(formElement));
-  teaserButton.addEventListener('click', (event) => navigateToUrl('https://www.iteratec.com'));
+    const submitButton = document.getElementById('submit-button')
+    submitButton.addEventListener('click', (event) => handleSubmit(formElement))
+    teaserButton.addEventListener('click', (event) =>
+        navigateToUrl('https://www.iteratec.com')
+    )
 
-  const modalElement =  document.getElementById('modal');
-  const closeModalButton =  document.getElementById('close-modal-btn');
-  closeModalButton.addEventListener('click', ()=> modalElement.classList.add('hidden') )
+    const modalElement = document.getElementById('modal')
+    const closeModalButton = document.getElementById('close-modal-btn')
+    closeModalButton.addEventListener('click', () =>
+        modalElement.classList.add('hidden')
+    )
 }
 
 async function handleSubmit(formElement) {
+    const formData = new FormData(formElement)
+    for (var [key, value] of formData.entries()) {
+        console.log(key, value)
+    }
 
-  const formData = new FormData(formElement);
-  for (var [key, value] of formData.entries()) {
-    console.log(key, value);
-  }
+    // await fetch('/contact', {
+    //   method: 'post',
+    //   body: formData,
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => showResult(data.message));
 
-  // await fetch('/contact', {
-  //   method: 'post',
-  //   body: formData,
-  // })
-  //   .then((response) => response.json())
-  //   .then((data) => showResult(data.message));
-
-  formElement.classList.add('hidden');
-  alert('submitting form')
+    formElement.classList.add('hidden')
+    alert('submitting form')
 }
 
 function showResult(text) {
-  const responseElement = document.createElement('div');
-  responseElement.innerHTML = text;
+    const responseElement = document.createElement('div')
+    responseElement.innerHTML = text
 
-  const mainElement = document.querySelector('main');
-  mainElement.appendChild(responseElement);
+    const mainElement = document.querySelector('main')
+    mainElement.appendChild(responseElement)
 }
 
 function toggleMenu() {
-  const navElement = document.querySelector('.navigation');
-  navElement.classList.toggle('menu-open');
+    const navElement = document.querySelector('.navigation')
+    navElement.classList.toggle('menu-open')
 }
 
 function navigateToUrl(url) {
-window.location.href = url
+    window.location.href = url
 }
